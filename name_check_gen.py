6d1b131e6f12afd2d7e2e825c37fff62335e1a56
LC Yake <lilycl@protonmail.com>
2020-09-28 18:23:20 -0600

    reformat source code

--- name_check_gen.py
+++ name_check_gen.py
@@ -13,10 +13,10 @@ with open(r'current_members/current_members.csv', mode='r') as csv_file:
         member_list.append(line)
 
     # Creates a csv file containing dummy personal information of people who registered twice by accident
-    # twice_enrolled = random.choices(member_list, k=10)
-    # with open('new_members/twice_enrolled.csv', mode='w', newline='') as new_file:
-    #     writer = csv.DictWriter(new_file, fieldnames=reader.fieldnames)
-    #     writer.writerows(twice_enrolled)
+    twice_enrolled = random.choices(member_list, k=10)
+    with open('new_members/twice_enrolled.csv', mode='w', newline='') as new_file:
+        writer = csv.DictWriter(new_file, fieldnames=reader.fieldnames)
+        writer.writerows(twice_enrolled)
 
 
 
