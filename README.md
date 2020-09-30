ecf32535ec4fd9fc08bcb4218f9d707dfd515c68
LC Yake <lilycl@protonmail.com>
2020-09-30 16:48:24 -0600

    Add records and duplicates

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 <h1>Learning to Work Smarter, Not Harder</h1>
-<p>Pythonic Automation: Catching duplicate member registration entries
+<p>Pythonic Automation: A script which references a collection of current members (1000 records), compares the list of 
+current members to a list of incoming members, and then the program returns the amount of people who have accidentally been 
+recorded more than once. 
 
 <br>
 <br>
-For a prettier version of my notebook, please use this <a href=https://nbviewer.jupyter.org/github/L-Yake/name_check/blob/master/name_check_nb.ipynb>link here</a>
+For a prettier version of this notebook, please use this <a href=https://nbviewer.jupyter.org/github/L-Yake/name_check/blob/master/name_check_nb.ipynb>link here</a>
 </p>
--- name_check_functions.py
+++ name_check_functions.py
@@ -10,10 +10,12 @@ def duplicate_check(key, title=False):
     :param title: Accepts a string as a title and creates a new CSV-file. Recommended format: organization-abbreviation_YYYY-MM-DD.csv
     :return: Returns list of duplicate entries and number of duplicate entries found.
     """
-    with open(r'current_members/current_members.csv', mode='r') as member_file:
+    # Searches collection of 1000 currently enrolled members
+    with open(r'current_members/current_members20203009.csv', mode='r') as member_file:
         current_members = csv.DictReader(member_file)
         current_members_list = list(current_members)
 
+        # Collection of newly registered members
         with open(r'new_members/new_members_09-20-2020.csv', mode='r') as new_member_file:
             new_members = csv.DictReader(new_member_file, fieldnames=current_members.fieldnames)
             new_members_list = list(new_members)
@@ -38,4 +40,4 @@ def duplicate_check(key, title=False):
                     print('New file created!')
 
 
-# duplicate_check('name', True)
+# duplicate_check('name', "ORG-ABBV_date")
